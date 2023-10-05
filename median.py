ae133a277ed3dbbbc9e8c38b3aece6f9b4c552fc
MelikeCeydaSenocak <melike_ceyda.senocak@kcl.ac.uk>
2023-10-05 12:01:02 +0100

    Modify median file

--- median.py
+++ median.py
@@ -1,6 +1,4 @@
 """Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +7,17 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+# Sort the list of numbers
+numbers.sort()
+
+# Calculate the median
+n = len(numbers)
+if n % 2 == 0:
+    # If the list has an even number of elements, the median is the average of the middle two elements
+    median = (numbers[n // 2 - 1] + numbers[n // 2]) / 2
+else:
+    # If the list has an odd number of elements, the median is the middle element
+    median = numbers[n // 2]
+
+print("Median:", median)
